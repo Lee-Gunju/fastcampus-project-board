a6c2034b6116142eccf4861c69c0ea8cfca17d42
DESKTOP-4BI04SL\Lee <hanyd1012@gmail.com>
2023-04-16 14:38:58 +0900

    #1 - data rest 설정

--- fastcampus-project-board-2/src/main/java/com/fastcampus/repository/ArticleCommentRepository.java
+++ fastcampus-project-board-2/src/main/java/com/fastcampus/repository/ArticleCommentRepository.java
@@ -3,6 +3,8 @@ package com.fastcampus.repository;
 import com.fastcampus.domain.Article;
 import com.fastcampus.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
